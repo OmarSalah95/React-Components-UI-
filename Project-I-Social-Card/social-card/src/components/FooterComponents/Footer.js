1f1b398f14542e9e0d231e83296ce3c911bfab61
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-02-11 16:49:35 -0800

    Imported react Icons and installed the library using NPM, and got the icons functioning finally

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,12 +1,48 @@
-import React from 'react';
+// import React from 'react';
 
-<footer className='footer'>
-  <img src=''/>
-  <img src=''/>
-  <img src=''/>
-  <img src=''/>
-</footer>
+// <footer className='footer'>
+//   <img src=''/>
+//   <img src=''/>
+//   <img src=''/>
+//   <img src=''/>
+// </footer>
 
 
 
-export default HeaderContainer;
+// export default HeaderContainer;
+
+
+
+import React from "react";
+import {FaComment, FaSync, FaHeart, FaEnvelope} from 'react-icons/fa'
+import "./Footer.css";
+
+// started to make a dynamic button here so when you click the image icons, the numbers beside them will increment
+// const DynamicButton = props => {
+
+// }
+
+const Footer = () => {
+  return (
+          <footer>
+            <div>
+             <FaComment />
+              1
+            </div>
+            <div>
+            <FaSync />
+              2
+            </div>
+            <div>
+              <FaHeart />
+              3
+            </div>
+            <div>
+             <FaEnvelope />
+              4
+            </div>
+          </footer>
+  );
+};
+
+export default Footer;
